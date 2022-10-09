636dc3eae0d8271e6d3d09c72d91c22d9291a153
Noob Coder <69426049+ranjit7858@users.noreply.github.com>
2022-10-10 00:10:29 +0530

    Update organize.js

--- file_system_organizer/commands/organize.js
+++ file_system_organizer/commands/organize.js
@@ -1,5 +1,11 @@
 let fs = require("fs");
 let path = require("path");
+let types = {
+    media: ["mp4", "mkv"],
+    archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', "xz"],
+    documents: ['docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex'],
+    app: ['exe', 'dmg', 'pkg', "deb"]
+};
 function organizeFn(dirPath){
     if(dirPath == undefined)
     {
